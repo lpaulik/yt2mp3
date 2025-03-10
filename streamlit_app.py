de514f81077b8321cbb331a81abf319529bae789
lpaulik <63648987+lpaulik@users.noreply.github.com>
2025-03-10 17:03:43 +0100

    yt-dlp code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import yt_dlp
 
 st.title("Youtube to mp3 converter")
+st.text("Drop a youtube link below and voila!")
+link = st.text_input("Youtube link", "https://www.youtube.com/watch?v=")
+
+def download_video_as_mp3(url, bitrate='192k'):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'noplaylist': True,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': bitrate,
+        }],
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+for videoid in videoids:
+    download_video_as_mp3(f'link')
