5770e5839dc9d81f479b0e306f337d0bde39281a
lpaulik <laszlo.paulik@hotmail.co.uk>
2025-03-11 08:05:32 +0100

    Add MP3 download functionality and new audio file

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import yt_dlp
+import os
+from pathlib import Path
 
 st.title("Youtube to mp3 converter")
 
@@ -10,9 +12,13 @@ st.text("Drop a youtube link below and voila")
 link = st.text_input("Youtube link")
 
 def download_video_as_mp3(url):
+    # Get the path to the user's Downloads folder
+    downloads_path = str(Path.home() / "Downloads")
+
     ydl_opts = {
         'format': 'bestaudio/best',
         'noplaylist': True,
+        'outtmpl': os.path.join(downloads_path, '%(title)s.%(ext)s'),
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -23,9 +29,10 @@ def download_video_as_mp3(url):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-mp3=download_video_as_mp3(f'link')
+if link:
+    if st.button("Download as MP3"):
+        download_video_as_mp3(link)
+        st.success("Download started!")
 
-st.download_button(
-    file_name="test.mp3",
-    data=mp3
-)
+
+#https://www.youtube.com/watch?v=VEfMAsFBMC8
