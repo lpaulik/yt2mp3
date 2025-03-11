9957ce79dca0e814ba477c7832ad2343175e6da3
lpaulik <laszlo.paulik@hotmail.co.uk>
2025-03-11 09:55:27 +0100

    Enhance MP3 conversion process by adding link clearing functionality and improving filename handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,10 @@ st.title("Youtube to mp3 converter")
 
 st.text("Drop a youtube link below and voila")
 
-link = st.text_input("Youtube link")
+link = st.text_input("Youtube link", value=None, key="link")
+
+def clear_link():
+    st.session_state.link = ""
 
 def download_video_as_mp3(url):
 
@@ -20,30 +23,36 @@ def download_video_as_mp3(url):
             'preferredcodec': 'mp3',
             'preferredquality': '192k',
         }],
+        'outtmpl': '%(title)s.%(ext)s',
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
 if link:
-    if st.button("Convert to mp3"):
+        
+        ytp = yt_dlp.YoutubeDL()
+        info_dict = ytp.extract_info(url=link, download=False)
+        video_title = info_dict.get('title', None)
+        filename = f"{video_title}.mp3"
+        
+        st.text(filename)
+        
         with st.spinner('Converting...'):
             download_video_as_mp3(link)
         st.success("File converted")
-
-        info_dict = yt_dlp.YoutubeDL.extract_info(url=link, download=False)
-        video_title = info_dict.get('title', None)
-        filename = f"{video_title}.mp3"
-        os.rename(f"{link.split('=')[-1]}.mp3", filename)
 
         st.download_button(
             label="Download mp3",
             data=open(filename, "rb").read(),
             file_name=filename,
-            mime="audio/mpeg"
+            mime="audio/mpeg",
+            icon=":material/download_for_offline:",
+            on_click=clear_link
         )
 
-        os.remove(f"{link.split('=')[-1]}.mp3")
+        os.remove(filename)
 
+        link = st.empty()
 
 #https://www.youtube.com/watch?v=ymgIkOXp6Ds
